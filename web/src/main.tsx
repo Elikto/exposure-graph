6f5bd54a7882bea5d9452ebd21fe04ce676fdd91
Elikto <105332200+Elikto@users.noreply.github.com>
2026-09-12 00:06:12 +0200

    Switch to dark ergonomic interface

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import AppV2 from './AppV2'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <AppV2 />
   </React.StrictMode>,
 )
